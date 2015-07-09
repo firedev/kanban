98bc803624f0ad3038872048792e6ad0dac21390
Nick Ostrovsky <nick@firedev.com>
2015-07-09 10:01:45 +0700

    Deleteable notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -37,7 +37,11 @@ class App extends React.Component {
 
   itemEdited(i, task) {
     var notes = this.state.notes;
-    notes[i].task = task;
+    if(task) {
+      notes[i].task = task;
+    } else {
+      notes = notes.slice(0, i).concat(notes.slice(i + 1));
+    }
     this.setState({
       notes: notes
     });
